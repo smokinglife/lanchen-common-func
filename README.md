67d16aa1b21b0a63d2de7cebf9d600a0e4a5d6a4
break <978952042@qq.com>
2023-10-23 19:02:29 +0800

    fist code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# lanchen-common-func
-lanchen company common method class
+# 蓝晨通用公共方法类
+安装方法:
+~~~
+composer require lc_common_func/kong
+~~~
+
+**不定期更新方法类,欢迎交流建议更新QQ:978952042@qq.com**
---
+++ src/Common/Client.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smokinglife
+ * Date: 2023/10/23
+ * Time: 18:47
+ */
+
+namespace Common\Client;
+
+
+class Client
+{
+    /**
+     * No explanation
+     *
+     * @author Zhiqiang Guo
+     * @return void
+     * @throws Exception
+     * @access public
+     */
+    public function index ()
+    {
+        echo "this is client";
+    }
+
+}
